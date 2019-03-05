c857d8fc4aea453c9adb234870460e236f10ca48
Jean Feydy <jean.feydy@gmail.com>
2019-03-05 14:56:02 +0100

    Refactor sinkhorn samples.

---
+++ geomloss/sinkhorn_divergence.py
@@ -0,0 +1,187 @@
+"""Implements the (unbiased) Sinkhorn divergence between abstract measures.
+
+.. math::
+    \\text{S}_{\\varepsilon,\\rho}(\\alpha,\\beta) 
+        ~&=~ \\text{OT}_{\\varepsilon,\\rho}(\\alpha, \\beta)
+         ~-~\\tfrac{1}{2} \\text{OT}_{\\varepsilon,\\rho}(\\alpha, \\alpha)
+         ~-~\\tfrac{1}{2} \\text{OT}_{\\varepsilon,\\rho}(\\beta, \\beta)
+         ~+~ \\tfrac{\\varepsilon}{2} \| \\langle \\alpha, 1\\rangle - \\langle \\beta, 1\\rangle \|^2
+
+where:
+
+.. math::
+    \\text{OT}_{\\varepsilon,\\rho}(\\alpha, \\beta)
+    ~&=~ \\min_{\pi\geqslant 0} \\langle\, \pi\,,\, \\text{C} \,\\rangle
+        ~+~\\varepsilon \, \\text{KL}(\pi,\\alpha\otimes\\beta) \\\\
+        ~&+~\\rho \, \\text{KL}(\pi\,\mathbf{1},\\alpha)
+        ~+~\\rho \, \\text{KL}(\pi^\intercal \,\mathbf{1},\\beta ) \\
+    &=~ \\max_{b,a} -\\rho \\langle\, \\alpha \,,\, e^{-b/\\rho} - 1\,\\rangle
+        -\\rho \\langle\, \\beta \,,\, e^{-a/\\rho} - 1\,\\rangle \\\\
+        &-~
+        \\epsilon \\langle\, \\alpha\otimes\\beta \,,\, e^{(b\oplus a - \\text{C})/\\epsilon} - 1\,\\rangle,
+
+with a Kullback-Leibler divergence defined through:
+
+.. math::
+    \\text{KL}(\\alpha, \\beta)~=~
+    \\langle \, \\alpha  \,,\, \\log \\tfrac{\\text{d}\\alpha}{\\text{d}\\beta} \,\\rangle
+    ~-~ \\langle \, \\alpha  \,,\, 1 \,\\rangle
+    ~+~ \\langle \, \\beta   \,,\, 1 \,\\rangle ~\geqslant~ 0.
+"""
+
+import numpy as np
+import torch
+from functools import partial
+
+try:  # Import the keops library, www.kernel-operations.io
+    from pykeops.torch import generic_logsumexp
+    from pykeops.torch.cluster import grid_cluster, cluster_ranges_centroids, sort_clusters, from_matrix
+    keops_available = True
+except:
+    keops_available = False
+    
+from .utils import scal, squared_distances, distances
+
+
+# ==============================================================================
+#                            ε-scaling heuristic
+# ==============================================================================
+
+
+def max_diameter(x, y):
+    mins = torch.stack((x.min(dim=0)[0], y.min(dim=0)[0])).min(dim=0)[0]
+    maxs = torch.stack((x.max(dim=0)[0], y.max(dim=0)[0])).max(dim=0)[0]
+    diameter = (maxs-mins).norm().item()
+    return diameter
+
+
+def epsilon_schedule(p, diameter, blur, scaling):
+    ε_s = [ diameter**p ] \
+        + [ np.exp(e) for e in np.arange(p*np.log(diameter), p*np.log(blur), p*np.log(scaling)) ] \
+        + [ blur**p ]
+    return ε_s
+
+
+def scaling_parameters( x, y, p, blur, reach, diameter, scaling):
+
+    if diameter is None:
+        D = x.shape[-1]
+        diameter = max_diameter(x.view(-1,D), y.view(-1,D))
+
+    ε   = blur**p
+    ε_s = epsilon_schedule( p, diameter, blur, scaling )
+    ρ   = None if reach is None else reach**p
+    return ε, ε_s, ρ  
+
+
+# ==============================================================================
+#                              Sinkhorn loop
+# ==============================================================================
+
+def dampening(ε, ρ):
+    return 1 if ρ is None else 1 / ( 1 + ε / ρ )
+
+
+def log_weights(α):
+    α_log = α.log()
+    α_log[α <= 0] = -100000
+    return α_log
+
+
+def sinkhorn_cost(ε, ρ, α, β, a_x, b_y, a_y, b_x, batch=False):
+    if ρ is None:
+        return scal( α, b_x - a_x, batch=batch ) + scal( β, a_y - b_y, batch=batch )
+
+
+def sinkhorn_loop( softmin, α_logs, β_logs, C_xxs, C_yys, C_xys, C_yxs, ε_s, ρ, 
+                   jumps=[], kernel_truncation=None, truncate=5,
+                   extrapolate=None, last_extrapolation=True ):
+    
+    Nits = len(ε_s)
+    if type(α_logs) is not list:
+        α_logs, β_logs = [α_logs], [β_logs]
+        C_xxs,  C_yys  = [C_xxs],  [C_yys]
+        C_xys,  C_yxs  = [C_xys],  [C_yxs]
+
+
+    torch.autograd.set_grad_enabled(False)
+
+    k = 0  # Scale index; we start at the coarsest resolution available
+    ε = ε_s[k] ; λ = dampening(ε_s[-1], ρ)
+
+    # Load the measures and cost matrices at the current scale:
+    α_log, β_log = α_logs[k], β_logs[k]
+    C_xx,  C_yy  = C_xxs[k],  C_yys[k]
+    C_xy,  C_yx  = C_xys[k],  C_yxs[k]
+
+    # Start with a decent initialization for the dual vectors:
+    a_x = λ * softmin(ε, C_xx, α_log )  # OT(α,α)
+    b_y = λ * softmin(ε, C_yy, β_log )  # OT(β,β)
+    a_y = λ * softmin(ε, C_yx, α_log )  # OT(α,β) wrt. a
+    b_x = λ * softmin(ε, C_xy, β_log )  # OT(α,β) wrt. b
+
+    for i, ε in enumerate(ε_s):  # ε-scaling descent -----------------------
+
+        # λ = dampening(ε, ρ)  # I don't know yet which one is better
+
+        # "Coordinate ascent" on the dual problems:
+        at_x = λ * softmin(ε, C_xx, α_log + a_x/ε )  # OT(α,α)
+        bt_y = λ * softmin(ε, C_yy, β_log + b_y/ε )  # OT(β,β)
+        at_y = λ * softmin(ε, C_yx, α_log + b_x/ε )  # OT(α,β) wrt. a
+        bt_x = λ * softmin(ε, C_xy, β_log + a_y/ε )  # OT(α,β) wrt. b
+
+        # Symmetrized updates:
+        a_x, b_y = .5 * ( a_x + at_x ), .5 * ( b_y + bt_y )  # OT(α,α), OT(β,β)
+        a_y, b_x = .5 * ( a_y + at_y ), .5 * ( b_x + bt_x )  # OT(α,β) wrt. a, b
+
+
+        if i in jumps:  # Jump from a coarse to a finer scale --------------
+
+            if i == len(ε_s) - 1:  # Last iteration: just extrapolate!
+
+                C_xx_, C_yy_ = C_xxs[k+1], C_yys[k+1]
+                C_xy_, C_yx_ = C_xys[k+1], C_yxs[k+1]
+
+                last_extrapolation = False  # No need to re-extrapolate after the loop
+                torch.autograd.set_grad_enabled( True )
+
+            else:  # It's worth investing some time on kernel truncation...
+
+                # Kernel truncation trick (described in Bernhard Schmitzer's 2016 paper),
+                # that typically relies on KeOps' block-sparse routines:
+                C_xx_, _     = kernel_truncation( C_xx, C_xx, C_xxs[k+1], C_xxs[k+1],
+                                                    a_x, a_x, ε, truncate=truncate)
+                C_yy_, _     = kernel_truncation( C_yy, C_yy, C_yys[k+1], C_yys[k+1],
+                                                    b_y, b_y, ε, truncate=truncate)
+                C_xy_, C_yx_ = kernel_truncation( C_xy, C_yx, C_xys[k+1], C_yxs[k+1],
+                                                    b_x, a_y, ε, truncate=truncate)
+
+
+            # Extrapolation for the symmetric problems:
+            a_x = extrapolate( a_x, a_x, ε, λ, C_xx, α_log, C_xx_ )
+            b_y = extrapolate( b_y, b_y, ε, λ, C_yy, β_log, C_yy_ )
+
+            # The cross-updates should be done in parallel!
+            a_y, b_x = extrapolate( a_y, b_x, ε, λ, C_yx, α_log, C_yx_ ), \
+                       extrapolate( b_x, a_y, ε, λ, C_xy, β_log, C_xy_ )
+
+
+            # Update the measure weights and cost "matrices":
+            k = k+1
+            α_log, β_log = α_logs[k], β_logs[k]
+            C_xx,  C_yy  = C_xx_,  C_yy_
+            C_xy,  C_yx  = C_xy_,  C_yx_
+
+    torch.autograd.set_grad_enabled(True)
+
+    if last_extrapolation:
+        # Last extrapolation, to get the correct gradients:
+        a_x = λ * softmin(ε, C_xx, (α_log + a_x/ε).detach() )
+        b_y = λ * softmin(ε, C_yy, (β_log + b_y/ε).detach() )
+
+        # The cross-updates should be done in parallel!
+        a_y, b_x = λ * softmin(ε, C_yx, (α_log + b_x/ε).detach() ), \
+                   λ * softmin(ε, C_xy, (β_log + a_y/ε).detach() )
+
+    return a_x, b_y, a_y, b_x
+
--- geomloss/sinkhorn_samples.py
+++ geomloss/sinkhorn_samples.py
@@ -1,33 +1,4 @@
-"""Implements the (unbiased) Sinkhorn divergence between sampled measures.
-
-.. math::
-    \\text{S}_{\\varepsilon,\\rho}(\\alpha,\\beta) 
-        ~&=~ \\text{OT}_{\\varepsilon,\\rho}(\\alpha, \\beta)
-         ~-~\\tfrac{1}{2} \\text{OT}_{\\varepsilon,\\rho}(\\alpha, \\alpha)
-         ~-~\\tfrac{1}{2} \\text{OT}_{\\varepsilon,\\rho}(\\beta, \\beta)
-         ~+~ \\tfrac{\\varepsilon}{2} \| \\langle \\alpha, 1\\rangle - \\langle \\beta, 1\\rangle \|^2
-
-where:
-
-.. math::
-    \\text{OT}_{\\varepsilon,\\rho}(\\alpha, \\beta)
-    ~&=~ \\min_{\pi\geqslant 0} \\langle\, \pi\,,\, \\text{C} \,\\rangle
-        ~+~\\varepsilon \, \\text{KL}(\pi,\\alpha\otimes\\beta) \\\\
-        ~&+~\\rho \, \\text{KL}(\pi\,\mathbf{1},\\alpha)
-        ~+~\\rho \, \\text{KL}(\pi^\intercal \,\mathbf{1},\\beta ) \\
-    &=~ \\max_{b,a} -\\rho \\langle\, \\alpha \,,\, e^{-b/\\rho} - 1\,\\rangle
-        -\\rho \\langle\, \\beta \,,\, e^{-a/\\rho} - 1\,\\rangle \\\\
-        &-~
-        \\epsilon \\langle\, \\alpha\otimes\\beta \,,\, e^{(b\oplus a - \\text{C})/\\epsilon} - 1\,\\rangle,
-
-with a Kullback-Leibler divergence defined through:
-
-.. math::
-    \\text{KL}(\\alpha, \\beta)~=~
-    \\langle \, \\alpha  \,,\, \\log \\tfrac{\\text{d}\\alpha}{\\text{d}\\beta} \,\\rangle
-    ~-~ \\langle \, \\alpha  \,,\, 1 \,\\rangle
-    ~+~ \\langle \, \\beta   \,,\, 1 \,\\rangle ~\geqslant~ 0.
-"""
+"""Implements the (unbiased) Sinkhorn divergence between sampled measures."""
 
 import numpy as np
 import torch
@@ -42,90 +13,9 @@ except:
     
 from .utils import scal, squared_distances, distances
 
+from .sinkhorn_divergence import epsilon_schedule, scaling_parameters
+from .sinkhorn_divergence import dampening, log_weights, sinkhorn_cost, sinkhorn_loop
 
-# ==============================================================================
-#                            ε-scaling heuristic
-# ==============================================================================
-
-def max_diameter(x, y):
-    mins = torch.stack((x.min(dim=0)[0], y.min(dim=0)[0])).min(dim=0)[0]
-    maxs = torch.stack((x.max(dim=0)[0], y.max(dim=0)[0])).max(dim=0)[0]
-    diameter = (maxs-mins).norm().item()
-    return diameter
-
-
-def epsilon_schedule(p, diameter, blur, scaling):
-    ε_s = [ diameter**p ] \
-        + [ np.exp(e) for e in np.arange(p*np.log(diameter), p*np.log(blur), p*np.log(scaling)) ] \
-        + [ blur**p ]
-    return ε_s
-
-
-def scaling_parameters( x, y, p, blur, reach, diameter, scaling):
-
-    if diameter is None:
-        D = x.shape[-1]
-        diameter = max_diameter(x.view(-1,D), y.view(-1,D))
-
-    ε   = blur**p
-    ε_s = epsilon_schedule( p, diameter, blur, scaling )
-    ρ   = None if reach is None else reach**p
-    return ε, ε_s, ρ  
-
-
-# ==============================================================================
-#                              Sinkhorn loop
-# ==============================================================================
-
-def dampening(ε, ρ):
-    return 1 if ρ is None else 1 / ( 1 + ε / ρ )
-
-
-def log_weights(α):
-    α_log = α.log()
-    α_log[α <= 0] = -100000
-    return α_log
-
-
-def sinkhorn_cost(ε, ρ, α, β, a_i, b_j, a_j, b_i, batch=False):
-    if ρ is None:
-        return scal( α, b_i - a_i, batch=batch ) + scal( β, a_j - b_j, batch=batch )
-
-
-def sinkhorn_loop( softmin, α, β, C_xx, C_yy, C_xy, C_yx, ε_s, ρ ):
-
-    with torch.no_grad():
-        ε = ε_s[0]
-        λ = dampening(ε, ρ)
-
-        α_log, β_log = log_weights(α), log_weights(β)
-        a_i = λ * softmin(ε, C_xx, α_log )  # (B,N)
-        b_j = λ * softmin(ε, C_yy, β_log )  # (B,M)
-        a_j = λ * softmin(ε, C_yx, α_log )  # (B,M)
-        b_i = λ * softmin(ε, C_xy, β_log )  # (B,N)
-
-        for ε in ε_s:
-            λ = dampening(ε, ρ)
-
-            # "Coordinate ascent" on the dual problems:
-            at_i = λ * softmin(ε, C_xx, α_log + a_i/ε )  # (B,N)
-            bt_j = λ * softmin(ε, C_yy, β_log + b_j/ε )  # (B,M)
-            at_j = λ * softmin(ε, C_yx, α_log + b_i/ε )  # (B,M)
-            bt_i = λ * softmin(ε, C_xy, β_log + a_j/ε )  # (B,N)
-
-            # Symmetrized updates:
-            a_i, b_j = .5 * ( a_i + at_i ), .5 * ( b_j + bt_j )  # OT(α,α), OT(β,β)
-            a_j, b_i = .5 * ( a_j + at_j ), .5 * ( b_i + bt_i )  # OT(α,β)
-
-    # Last extrapolation, to get the correct gradients:
-    a_i = λ * softmin(ε, C_xx, (α_log + a_i/ε).detach() )  # (B,N)
-    b_j = λ * softmin(ε, C_yy, (β_log + b_j/ε).detach() )  # (B,M)
-
-    # The cross-updates should be done in parallel!
-    a_j, b_i = λ * softmin(ε, C_yx, (α_log + b_i/ε).detach() ), \
-               λ * softmin(ε, C_xy, (β_log + a_j/ε).detach() )
-
-    return a_i, b_j, a_j, b_i
 
 
 # ==============================================================================
@@ -155,10 +45,11 @@ def sinkhorn_tensorized(α, x, β, y, p=2, blur=.05, reach=None, diameter=None, 
 
     ε, ε_s, ρ = scaling_parameters( x, y, p, blur, reach, diameter, scaling )
 
-    a_i, b_j, a_j, b_i = sinkhorn_loop( softmin_tensorized, α, β, 
+    a_x, b_y, a_y, b_x = sinkhorn_loop( softmin_tensorized, 
+                                        log_weights(α), log_weights(β), 
                                         C_xx, C_yy, C_xy, C_yx, ε_s, ρ )
 
-    return sinkhorn_cost(ε, ρ, α, β, a_i, b_j, a_j, b_i, batch=True)
+    return sinkhorn_cost(ε, ρ, α, β, a_x, b_y, a_y, b_x, batch=True)
 
 
 # ==============================================================================
@@ -170,9 +61,20 @@ cost_formulas = {
     2 : "(SqDist(X,Y) / IntCst(2))",
 }
 
-def softmin_online(ε, C_ij, f_j, log_conv=None):
-    x_i, y_j = C_ij
-    return - ε * log_conv( x_i, y_j, f_j.view(-1,1), torch.Tensor([1/ε]).type_as(x_i) ).view(-1)
+def softmin_online(ε, C_xy, f_y, log_conv=None):
+    x, y = C_xy
+    # KeOps is pretty picky on the input shapes...
+    return - ε * log_conv( x, y, f_y.view(-1,1), torch.Tensor([1/ε]).type_as(x) ).view(-1)
+
+
+def softmin_keops(cost, D):
+    log_conv = generic_logsumexp("( B - (P * " + cost + " ) )",
+                                 "A = Vx(1)",
+                                 "X = Vx({})".format(D),
+                                 "Y = Vy({})".format(D),
+                                 "B = Vy(1)",
+                                 "P = Pm(1)")
+    return partial(softmin_online, log_conv=log_conv)
 
 
 def sinkhorn_online(α, x, β, y, p=2, blur=.05, reach=None, diameter=None, scaling=.5, cost=None, **kwargs):
@@ -183,14 +85,7 @@ def sinkhorn_online(α, x, β, y, p=2, blur=.05, reach=None, diameter=None, scal
     if cost is None:
         cost = cost_formulas[p]
 
-    log_conv = generic_logsumexp("( B - (P * " + cost + " ) )",
-                                 "A = Vx(1)",
-                                 "X = Vx({})".format(D),
-                                 "Y = Vy({})".format(D),
-                                 "B = Vy(1)",
-                                 "P = Pm(1)")
-
-    softmin = partial(softmin_online, log_conv=log_conv)
+    softmin = softmin_keops(cost, D)
 
     # The "cost matrices" are implicitely encoded in the point clouds,
     # and re-computed on-the-fly:
@@ -199,7 +94,15 @@ def sinkhorn_online(α, x, β, y, p=2, blur=.05, reach=None, diameter=None, scal
 
     ε, ε_s, ρ = scaling_parameters( x, y, p, blur, reach, diameter, scaling )
 
-    a_i, b_j, a_j, b_i = sinkhorn_loop( softmin, α, β, 
+    a_x, b_y, a_y, b_x = sinkhorn_loop( softmin,
+                                        log_weights(α), log_weights(β), 
                                         C_xx, C_yy, C_xy, C_yx, ε_s, ρ )
 
-    return sinkhorn_cost(ε, ρ, α, β, a_i, b_j, a_j, b_i)
+    return sinkhorn_cost(ε, ρ, α, β, a_x, b_y, a_y, b_x)
+
+
+
+# ==============================================================================
+#                          backend == "multiscale"
+# ==============================================================================
+
