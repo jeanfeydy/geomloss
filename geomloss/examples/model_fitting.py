8b6acd05af7d2fe3a6e8ba31417924c3635b4cff
Jean Feydy <jean.feydy@gmail.com>
2019-03-08 22:45:51 +0100

    Worked  on the Wasserstein barycenters.

---
+++ geomloss/examples/model_fitting.py
@@ -0,0 +1,139 @@
+"""
+Wrapper around the scipy.optimize module
+============================================
+"""
+
+import os
+import torch
+import matplotlib
+import matplotlib.pyplot as plt
+from math import isnan
+import numpy as np
+from scipy.optimize import minimize
+
+import warnings
+warnings.filterwarnings("ignore",".*GUI is implemented.*") # annoying warning with pyplot and pause...
+
+def mypause(interval):
+    """Pause matplotlib without stealing focus."""
+    backend = plt.rcParams['backend']
+    if backend in matplotlib.rcsetup.interactive_bk:
+        figManager = matplotlib._pylab_helpers.Gcf.get_active()
+        if figManager is not None:
+            canvas = figManager.canvas
+            if canvas.figure.stale:
+                canvas.draw()
+            canvas.start_event_loop(interval)
+            return
+
+
+def model_to_numpy(model, grad=False) :
+    """
+    The fortran routines used by scipy.optimize expect float64 vectors
+    instead of the gpu-friendly float32 matrices: we need conversion routines.
+    """
+    if not all( param.is_contiguous() for param in model.parameters() ) :
+        raise ValueError("Scipy optimization routines are only compatible with parameters given as *contiguous* tensors.")
+
+    if grad :
+        tensors = [param.grad.data.view(-1).cpu().numpy() for param in model.parameters()]
+    else :
+        tensors = [param.data.view(-1).cpu().numpy()      for param in model.parameters()]
+    return np.ascontiguousarray( np.hstack(tensors) , dtype='float64' )
+
+def numpy_to_model(model, vec) :
+    i = 0
+    for param in model.parameters() :
+        offset = param.numel()
+        param.data = torch.from_numpy(vec[i:i+offset]).view(param.data.size()).type(param.data.type())
+        i += offset
+
+    if i != len(vec) :
+        raise ValueError("The total number of variables in model is not the same as in 'vec'.")
+
+def fit_model(Model, method = "L-BFGS", tol = 1e-10, nits = 500, nlogs = 10,
+                     lr = .1, eps = .01, maxcor = 10, gtol = 1e-10, 
+                     verbose = False, **params) :
+    """
+    """
+
+    # Load parameters =====================================================================================================
+
+
+    # We'll minimize the model's cost
+    # with respect to the model's parameters using a standard gradient-like
+    # descent scheme. As we do not perform any kind of line search, 
+    # this algorithm may diverge if the learning rate is too large !
+    # For robust optimization routines, you may consider using
+    # the scipy.optimize API with a "parameters <-> float64 vector" wrapper.
+    use_scipy = False
+    if method == "Adam" :
+        optimizer = torch.optim.Adam(Model.parameters(), lr=lr, eps=eps)
+    elif method == "L-BFGS" :
+        optimizer = torch.optim.SGD(Model.parameters(), lr=1.) # We'll just use its "zero_grad" method...
+
+        use_scipy = True
+        method    = 'L-BFGS-B'
+        options   = dict( maxiter = nits,
+                          ftol    = tol,          # Don't bother fitting the shapes to float precision
+                          gtol    = gtol,
+                          maxcor  = maxcor        # Number of previous gradients used to approximate the Hessian
+                    )
+    else :
+        raise NotImplementedError('Optimization method not supported : "'+method+'". '\
+                                  'Available values are "Adam" and "L-BFGS".')
+
+    costs = []
+    # Define the "closures" associated to our model =======================================================================
+
+    fit_model.nit = -1 ; fit_model.breakloop = False
+    def closure(final_it=False):
+        """
+        Encapsulates a problem + display iteration into a single callable statement.
+        This wrapper is needed if you choose to use LBFGS-like algorithms, which
+        (should) implement a careful line search along the gradient's direction.
+        """
+        fit_model.nit += 1 ; it = fit_model.nit
+        # Minimization loop --------------------------------------------------------------------
+        optimizer.zero_grad()                      # Reset the gradients (PyTorch syntax...).
+        cost = Model.forward()
+        costs.append(cost.item())  # Store the "cost" for plotting.
+        cost.backward()            # Backpropagate to compute the gradient.
+        # Break the loop if the cost's variation is below the tolerance param:
+        if ( len(costs)>1 and abs(costs[-1]-costs[-2]) < tol ) or fit_model.nit == nits-1:
+            fit_model.breakloop = True
+        
+        if verbose :
+
+            Model.plot(nit=fit_model.nit, cost=cost.item())
+            # print("{}: {:2.4f}".format(fit_model.nit, cost.item()))
+        return cost
+    
+    # Scipy-friendly wrapper ------------------------------------------------------------------------------------------------
+    def numpy_closure(vec, final_it=False) :
+        """
+        Wraps the PyTorch closure into a 'float64'-vector routine,
+        as expected by scipy.optimize.
+        """
+        vec    = lr * vec.astype('float64')            # scale the vector, and make sure it's given as float64
+        numpy_to_model(Model, vec)                     # load this info into Model's parameters
+        c      = closure(final_it).item()              # compute the cost and accumulate the gradients wrt. the parameters
+        dvec_c = lr * model_to_numpy(Model, grad=True) # -> return this gradient, as a properly rescaled numpy vector
+        return (c, dvec_c)
+
+    # Actual minimization loop ===============================================================================================
+    if use_scipy :
+        res = minimize( numpy_closure,      # function to minimize
+                model_to_numpy(Model), # starting estimate
+                method  = method,
+                jac     = True,             # matching_problems also returns the gradient
+                options = options    )
+        numpy_closure(res.x, final_it=True) 
+        print(res.message)
+    else :
+        for i in range(nits+1) :            # Fixed number of iterations
+            optimizer.step(closure)         # "Gradient descent" step.
+            if fit_model.breakloop :
+                closure(final_it=True) 
+                break
+            
--- geomloss/examples/plot_wasserstein_barycenters_1D.py
+++ geomloss/examples/plot_wasserstein_barycenters_1D.py
@@ -20,23 +20,11 @@ import time
 
 import torch
 from geomloss import SamplesLoss
+from geomloss.examples.model_fitting import fit_model
+from torch.nn import Module, Parameter
 
 use_cuda = torch.cuda.is_available()
 dtype    = torch.cuda.FloatTensor if use_cuda else torch.FloatTensor
-
-###############################################
-# Display routine
-# ~~~~~~~~~~~~~~~~~
- 
-t_plot = np.linspace(-0.1, 1.1, 1000)[:,np.newaxis]
-
-def display_samples(ax, x, color):
-    """Displays samples on the unit interval using a density curve."""
-    kde  = KernelDensity(kernel='gaussian', bandwidth= .002 ).fit(x.data.cpu().numpy())
-    dens = np.exp( kde.score_samples(t_plot) )
-    dens[0] = 0 ; dens[-1] = 0
-    ax.fill(t_plot, dens, color=color)
-
 
 ###############################################
 # Dataset
@@ -53,89 +41,138 @@ def display_samples(ax, x, color):
 #   \beta  ~=~ \frac{1}{M}\sum_{j=1}^M \delta_{y_j}, ~~~
 #   \gamma  ~=~ \frac{1}{N}\sum_{k=1}^N \delta_{z_k}.
 
-N, M = (500, 500) if not use_cuda else (10000, 10000)
+N, M = (100, 100) if not use_cuda else (1000, 1000)
  
 t_i = torch.linspace(0, 1, M).type(dtype).view(-1,1)
 t_j = torch.linspace(0, 1, M).type(dtype).view(-1,1)
 t_k = torch.linspace(0, 1, N).type(dtype).view(-1,1)
 
-X_i, Y_j, Z_k = .1 * t_i, .1 * t_j + .9, t_k
+X_i, Y_j, Z_k = .1 * t_i, .2 * t_j + .8, t_k
+
+
 
 ###############################################
-# Wasserstein barycenter
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# 
+# Display routine
+# ~~~~~~~~~~~~~~~~~
  
-def lagrangian_barycenter(loss, w=.5, lr=.1) :
-    """Flows along the gradient of the cost function with respect to the
-    particles' positions, using a simple Euler scheme.
-    
-    Parameters:
-        loss ((a_i,x_i,b_j,y_j) -> torch float number): 
-            Real-valued loss function.
-        w (float, default=.5):
-            Interpolation parameter between the leftmost and rightmost measures.
-        lr (float, default = .1):
-            Learning rate, i.e. time step.
-    """
-    
-    # Parameters for the gradient descent
-    Nsteps = int(5/lr)+1 
-    display_its = [int(t/lr) for t in [0, 1., 5.]]
-    
-    # Make sure that we won't modify the reference samples
-    x_i, y_j, z_k = X_i.clone(), Y_j.clone(), Z_k.clone()
+t_plot = np.linspace(-0.1, 1.1, 1000)[:,np.newaxis]
 
-    # We're going to perform gradient descent on w*Loss(x_i,y_j) + (1-w)*Loss(x_i,z_k))
-    # wrt. the positions x_i of the diracs masses that make up our source measure:
-    z_k.requires_grad = True  
+def display_samples(ax, x, color, weights=None):
+    """Displays samples on the unit interval using a density curve."""
+    kde  = KernelDensity(kernel='gaussian', bandwidth= .002 ).fit(
+            x.data.cpu().numpy(), 
+            sample_weight = None if weights is None else weights.data.cpu().numpy())
+    dens = np.exp( kde.score_samples(t_plot) )
+    dens[0] = 0 ; dens[-1] = 0
+    ax.fill(t_plot, dens, color=color)
+
+
+class Barycenter(Module):
+    """Abstract model that will be optimized."""
     
-    t_0 = time.time()
-    plt.figure(figsize=(12,4)) ; k = 1
-    for i in range(Nsteps): # Euler scheme ===============
-        # Compute cost and gradient
-        L_γ  = w * loss(z_k, x_i) + (1 - w) * loss(z_k, y_j)
-        [g]  = torch.autograd.grad(L_γ, [z_k])
+    def __init__(self, loss, w=.5):
+        super(Barycenter, self).__init__()
+        self.loss = loss
+        self.w = w
+        self.x_i, self.y_j, self.z_k = X_i.clone(), Y_j.clone(), Z_k.clone()
 
-        if i in display_its : # display
-            ax = plt.subplot(1,3,k) ; k = k+1
+    def optimize(self):
+        fit_model(self, method = "L-BFGS", lr = 1., verbose = True)
 
-            display_samples(ax, x_i, (.95,.55,.55))
-            display_samples(ax, y_j, (.55,.55,.95))
-            display_samples(ax, z_k, (.55,.95,.55))
+    def weights(self):
+        return (torch.ones(len(self.z_k)) / len(self.z_k)).type_as(self.z_k)
+    
+    def plot(self, nit=0, cost=0):
+        if nit == 0 or nit % 16 == 4: 
+            plt.pause(.01)
+            plt.figure(figsize=(16,4))
+
+        if nit <= 4 or nit % 4 == 0:
+            if nit < 4: index = nit + 1
+            else:       index = (nit//4 - 1) % 4 + 1
+            ax = plt.subplot(1,4, index)
+            display_samples(ax, self.x_i, (.95,.55,.55))
+            display_samples(ax, self.y_j, (.55,.55,.95))
+            display_samples(ax, self.z_k, (.55,.95,.55), weights = self.weights())
             
-            ax.set_title("t = {:1.2f}".format(lr*i))
-            plt.axis([-.1,1.1,-.1,10.5])
+            ax.set_title("nit = {}, cost = {:3.4f}".format(nit, cost))
+            
+            plt.axis([-.1,1.1,-.1,20.5])
             plt.xticks([], []); plt.yticks([], [])
             plt.tight_layout()
-        
-        # in-place modification of the tensor's values
-        z_k.data -= lr * len(z_k) * g 
-    plt.title("t = {:1.2f}, elapsed time: {:.2f}s/it".format(lr*i, (time.time() - t_0)/Nsteps ))
-
 
 
 ###############################################
-# Wasserstein-2 distance
-# ~~~~~~~~~~~~~~~~~~~~~~~~
+# Lagrangian gradient flow
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 
+
+class LagrangianBarycenter(Barycenter) :
+    def __init__(self, loss, w=.5) :
+        super(LagrangianBarycenter, self).__init__(loss, w)
+        self.z_k = Parameter( Z_k.clone() )
+      
+    def forward(self) :
+        """Returns the cost to minimize."""
+        return self.w  * self.loss( self.z_k, self.x_i) \
+        + (1 - self.w) * self.loss( self.z_k, self.y_j)
+    
+
+###############################################
+# Blabla
 #
 
-lagrangian_barycenter( SamplesLoss("sinkhorn", blur=.1, scaling=.9), w=.5 )
+LagrangianBarycenter( SamplesLoss("sinkhorn", blur=.1, scaling=.9) ).optimize()
 
 ###############################################
 # Blabla
 
-lagrangian_barycenter( SamplesLoss("sinkhorn", blur=.01, scaling=.9), w=.5 )
+LagrangianBarycenter( SamplesLoss("sinkhorn", blur=.01, scaling=.9) ).optimize()
 
 ###############################################
 # Blabla
 
-lagrangian_barycenter( SamplesLoss("sinkhorn", blur=.001, scaling=.9), w=.5 )
+LagrangianBarycenter( SamplesLoss("sinkhorn", blur=.001, scaling=.9) ).optimize()
+
+ 
+###############################################
+# Eulerian gradient flow
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 
+
+from torch.nn.functional import conv1d
+
+def gaussian_conv(x, blur=2):
+    D = torch.arange( - 5*blur, 5*blur+1 ).type_as(x)
+    k_ε = (-D**2 / (2*blur**2)).exp()
+    return conv1d( x.view(1,1,-1), k_ε.view(1,1,-1), padding=k_ε.shape[-1]//2 ).view(-1)
+
+
+class EulerianBarycenter(Barycenter) :
+    def __init__(self, loss, w=.5) :
+        super(EulerianBarycenter, self).__init__(loss, w)
+
+        # We're going to work with weights:
+        self.a_i = (torch.ones(len(self.x_i)) / len(self.x_i)).type_as(self.x_i)
+        self.b_j = (torch.ones(len(self.y_j)) / len(self.y_j)).type_as(self.y_j)
+
+        # Our parameter to optimize: the logarithms of our weights
+        self.l_k = Parameter( torch.zeros(len(self.z_k)).type_as(self.z_k) )
+ 
+    def weights(self):
+        soft_lk = gaussian_conv( self.l_k, blur= int(.02*len(self.l_k)  ))
+        return torch.nn.functional.softmax(soft_lk, dim=0)
+      
+    def forward(self) :
+        """Returns the cost to minimize."""
+        c_k  = self.weights()
+        return self.w  * self.loss(c_k, self.z_k, self.a_i, self.x_i) \
+        + (1 - self.w) * self.loss(c_k, self.z_k, self.b_j, self.y_j)
 
 
 ###############################################
 # Blabla
 
-lagrangian_barycenter( SamplesLoss("sinkhorn", blur=.001, scaling=.5), w=.5 )
+EulerianBarycenter( SamplesLoss("sinkhorn", blur=.001, scaling=.9) ).optimize()
 
 plt.show()
