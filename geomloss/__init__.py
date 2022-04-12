b8d918bfc54b7b061cc33f9ecc08a4065c07ce8a
josephenguehard <josephenguehard@gmail.com>
2022-04-12 10:01:46 +0100

    Include version in init

--- geomloss/__init__.py
+++ geomloss/__init__.py
@@ -3,5 +3,6 @@ import sys, os.path
 from .samples_loss import SamplesLoss
 from .wasserstein_barycenter_images import ImagesBarycenter
 from .sinkhorn_images import sinkhorn_divergence
+from .__version__ import __version__
 
 __all__ = sorted(["SamplesLoss, ImagesBarycenter"])
