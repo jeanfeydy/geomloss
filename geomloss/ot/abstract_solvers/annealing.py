4d96a398ab8265319954795d414160c93119881b
Jean Feydy <jean.feydy@gmail.com>
2022-06-17 09:55:06 +0000

    Experimenting with types.

--- geomloss/ot/abstract_solvers/annealing.py
+++ geomloss/ot/abstract_solvers/annealing.py
@@ -6,14 +6,27 @@ Geometric data analysis, beyond convolutions (2020),
 https://www.jeanfeydy.com/geometric_data_analysis.pdf
 """
 
+from typing import List, Dict
+from numpy.typing import ArrayLike
+from collections.abc import Callable
+from collections import NamedTuple
 
+Tensor = ArrayLike
 
+class AnnealingParameters(NamedTuple):
+	diameter: float
+	blur: float
+	blur_list: List[float]
+	eps: float
+	eps_list: List[float]
+	rho: float
+	
 # ==============================================================================
 #                            eps-scaling heuristic
 # ==============================================================================
 
 
-def max_diameter(x, y):
+def max_diameter(x: Tensor, y: Tensor) -> float:
     """Returns a rough estimation of the diameter of a pair of point clouds.
 
     This quantity is used as a maximum "starting scale" in the epsilon-scaling
@@ -32,7 +45,7 @@ def max_diameter(x, y):
     return diameter
 
 
-def epsilon_schedule(p, diameter, blur, scaling):
+def epsilon_schedule(*, p, diameter, blur, n_iter=None, scaling=None):
     r"""Creates a list of values for the temperature "epsilon" across Sinkhorn iterations.
 
     We use an aggressive strategy with an exponential cooling
@@ -41,6 +54,9 @@ def epsilon_schedule(p, diameter, blur, scaling):
     by :math:`\text{scaling}^p` at every iteration until reaching
     a minimum value of :math:`\text{blur}^p`.
 
+	The number of iterations can be specified in two different ways,
+	using either an integer number (n_iter) or a ratio between successive scales(scaling).
+	
     Args:
         p (integer or float): The exponent of the Euclidean distance
             :math:`\|x_i-y_j\|` that defines the cost function
@@ -52,6 +68,8 @@ def epsilon_schedule(p, diameter, blur, scaling):
         blur (float, positive): Target value for the entropic regularization
             (":math:`\varepsilon = \text{blur}^p`").
 
+		n_iter (int): Number of iterations.	
+			
         scaling (float, in (0,1)): Ratio between two successive
             values of the blur scale.
 
@@ -61,9 +79,9 @@ def epsilon_schedule(p, diameter, blur, scaling):
     eps_list = (
         [diameter ** p]
         + [
-            np.exp(e)
+            np.exp(p * e)
             for e in np.arange(
-                p * np.log(diameter), p * np.log(blur), p * np.log(scaling)
+                np.log(diameter), np.log(blur), np.log(scaling)
             )
         ]
         + [blur ** p]
@@ -71,9 +89,19 @@ def epsilon_schedule(p, diameter, blur, scaling):
     return eps_list
 
 
-def scaling_parameters(x, y, p, blur, reach, diameter, scaling):
+def annealing_parameters(*, 
+	x: Tensor, 
+	y: Tensor, 
+	p: int, 
+	blur: float, 
+	reach: float, 
+	diameter=None: float,
+	n_iter=None: int,	
+	scaling=None: float,
+	) -> ScalingParameters :
     r"""Turns high-level arguments into numerical values for the Sinkhorn loop."""
-    if diameter is None:
+    
+	if diameter is None:
         D = x.shape[-1]
         diameter = max_diameter(x.view(-1, D), y.view(-1, D))
 
