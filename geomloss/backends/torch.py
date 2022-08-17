Finally implemented the correct formula for OT_e,r.

geomloss/backends/torch.py
@@ -108,28 +108,17 @@ def set_grad_enabled(typical_array, b):
     return torch.set_grad_enabled(b)
 
 
-class UnbalancedWeight(torch.nn.Module):
-    """Applies the correct scaling to the dual variables in the Sinkhorn divergence formula.
-
-    Remarkably, the exponentiated potentials should be scaled
-    by "rho + eps/2" in the forward pass and "rho + eps" in the backward.
-    For an explanation of this surprising "inconsistency"
-    between the forward and backward formulas,
-    please refer to Proposition 12 (Dual formulas for the Sinkhorn costs)
-    in "Sinkhorn divergences for unbalanced optimal transport",
-    Sejourne et al., https://arxiv.org/abs/1910.12958.
-    """
-
-    def __init__(self, *, eps: float, rho: float):
-        super(UnbalancedWeight, self).__init__()
-        self.eps, self.rho = eps, rho
+class ScaleForwardBackward(torch.nn.Module):
+    def __init__(self, *, forward: Any, backward: Any):
+        super(ScaleForwardBackward, self).__init__()
+        self.fw, self.bw = forward, backward
 
     def forward(self, x: RealTensor):
-        return (self.rho + self.eps / 2) * x
+        return self.fw * x
 
     def backward(self, g: RealTensor):
-        return (self.rho + self.eps) * g
+        return self.bw * g
 
 
-def unbalanced_weight(f, *, eps, rho):
-    return UnbalancedWeight(eps=eps, rho=rho)(f)
+def scale(f, *, forward, backward):
+    return ScaleForwardBackward(forward=forward, backward=backward)(f)

geomloss/ot/tests/gaussians.py
@@ -504,7 +504,7 @@ def gaussians_matrix(
     if True:
         means = np.random.rand(2, B, D)
     else:
-        means = np.array([[[0.]], [[1.]]])
+        means = np.array([[[0.0]], [[1.0]]])
     min_std = 3 * 3 / min(N, M)  # Typical distance between samples in 3/N
     # Make sure that the support of our Gaussians at +-5*sigma is in [-1,1]:
     max_std = 0.2
@@ -514,7 +514,7 @@ def gaussians_matrix(
     if True:
         total_mass = 2 * np.random.rand(2, B)  # (2, B)
     else:
-        total_mass = np.ones((2, B))
+        total_mass = 0.5 * np.ones((2, B))
 
     # TODO: Add non-diagonal test cases
     if cov_type == "diagonal":
@@ -592,7 +592,7 @@ def gaussians_matrix(
             value[k] = OT_sigma_gamma(sigma=blur, gamma=reach**2, **source_target(k))
             plan_k = pi_sigma_gamma(sigma=blur, gamma=reach**2, **source_target(k))
             plan[k, :, :] = plan_k(x=x_i, y=y_j)  # (N**D, M**D)
-        
+
         marginal_a = np.sum(plan, axis=2)
         marginal_b = np.sum(plan, axis=1)
 