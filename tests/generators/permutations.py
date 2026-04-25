6d2c7477055805320d9a30afb17727762bc49646
Jean Feydy <jean.feydy@gmail.com>
2026-04-25 22:13:44 +0200

    Lower test_permutations precision to 5%?

--- tests/generators/permutations.py
+++ tests/generators/permutations.py
@@ -71,7 +71,7 @@ def st_permutations_matrix(draw):
             C=C,
             maxiter=100,
             reg=1e-1,
-            atol=1e-2,
+            atol=5e-2,
             rtol=5e-2,
             result=ExpectedOTResult(
                 value=value,
